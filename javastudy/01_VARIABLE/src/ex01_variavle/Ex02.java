1b806dd83a47d63418b9ce7d483152518223b164
on2ul <1226_95@naver.com>
2022-07-20 09:17:00 +0900

    2022-07-20

---
+++ javastudy/01_VARIABLE/src/ex01_variavle/Ex02.java
@@ -0,0 +1,21 @@
+package ex01_variavle;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		// 문자열 타입
+		// 기본 타입(Primitive type)은 아니다.
+		// 자주 사용하기 때문에 기본 타입처럼 사용할 수 딨도록 자바가 지원한다.
+		// String 클래스를 사용한다.
+		
+		String contry="korea"; //자바가 쉬운 사용법을 지원
+		String name = new String("민경태"); //원래 사용 법
+		System.out.println(contry);
+		System.out.println(name);
+		
+	
+
+	}
+
+}
