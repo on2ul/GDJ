e00b7262301fcc6e2a3ff7265a56d0d4c9be0f88
tapetwo <1226_95@naver.com>
2022-10-28 17:46:38 +0900

    2022-10-28

---
+++ springstudy/01_ioc_container/src/main/java/com/gdu/app01/java03/Book.java
@@ -0,0 +1,33 @@
+package com.gdu.app01.java03;
+
+public class Book {
+
+	
+		// field
+		private String title;
+		private String author;
+		private Publisher publisher;
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getAuthor() {
+			return author;
+		}
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+		public Publisher getPublisher() {
+			return publisher;
+		}
+		public void setPublisher(Publisher publisher) {
+			this.publisher = publisher;
+		}
+		
+		// Getter / Setter
+		
+		
+	
+}
